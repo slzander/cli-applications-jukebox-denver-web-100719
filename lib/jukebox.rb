835e8c32d2c6c1b9289868fcb8fc01bd93276d39
Stacey Zander <staceyzander@gmail.com>
2019-10-02 03:34:56 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -32,7 +32,7 @@ def play(songs)
   response = gets.strip
   if songs.include?(response)
     puts "Playing #{response}"
-  elsif songs[response.to_i - 1]
+  elsif response.to_i > 0 
     puts "Playing #{songs[response.to_i - 1]}"
   else
     puts "Invalid input, please try again"
